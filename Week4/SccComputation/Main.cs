d1f964c35c069d104562089ce30b83d0d730c375
Djamel <djamelz@gmail.com>
2013-03-17 21:46:58 +0100

    Week4 .NET STYLEZ

---
+++ Week4/SccComputation/Main.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace SccComputation
+{
+    public class Main
+    {
+        public int[] ComputeScc(string filePath)
+        {
+            Dictionary<string, DfsItem> graph = new Dictionary<string, DfsItem>();
+            Dictionary<string, DfsItem> graphRev = new Dictionary<string, DfsItem>();
+            string[] values;
+            DfsItem dfsItem;
+            Stack<int> sccSize = new Stack<int>();
+
+            using (StreamReader sr = new StreamReader(filePath))
+            {
+                while (sr.Peek() >= 0)
+                {
+                    values = sr.ReadLine().Split(' ');
+                    if (graph.TryGetValue(values[0], out dfsItem))
+                        dfsItem.AddEdge(values[1]);
+                    else
+                        graph.Add(values[0], new DfsItem(values[0], values[1]));
+
+                    if (graphRev.TryGetValue(values[1], out dfsItem))
+                        dfsItem.AddEdge(values[0]);
+                    else
+                        graphRev.Add(values[1],new DfsItem(values[1], values[0]));
+                }
+            }
+
+            DfsLoop(graph, graphRev, sccSize);
+            int[] a = sccSize.Where(x => x > 0).OrderByDescending(x => x).Take(5).ToArray();
+            
+            Array.Resize<int>(ref a, 5);
+            
+            //backtesting
+            //sccSize = new Stack<int>();
+            //DfsLoop(graphRev, graph, sccSize);
+            //int[] b = sccSize.Where(x => x > 0).OrderByDescending(x => x).Take(5).ToArray();
+            //Array.Resize<int>(ref b, 5);
+            
+            return a;
+        }
+
+        private void DfsLoop(Dictionary<string, DfsItem> graph, Dictionary<string, DfsItem> graphRev, Stack<int> sccSize)
+        {
+            Stack<string> order = new Stack<string>();
+            Stack<string> sccMember;
+            Stack<int> fakeS = new Stack<int>();
+            DfsItem dfsItem;
+            HashSet<string> visited = new HashSet<string>();
+
+            foreach (var node in graphRev)
+            {
+                if ( !visited.Contains(node.Key) )
+                {
+                    DfsStack(graphRev, node.Value, order, 0, visited);
+                }
+            }
+
+            visited = new HashSet<string>();
+            foreach (var node in order)
+            {
+
+                if (graph.TryGetValue(node, out dfsItem))
+                {
+                    sccMember = new Stack<string>();
+                    if (!visited.Contains(node))
+                    {
+                        sccSize.Push(DfsStack(graph, dfsItem, sccMember, 0, visited));
+                    }
+                }
+                else
+                {
+                    visited.Add(node);
+                    sccSize.Push(1);
+                }
+            }
+            return;
+        }
+
+        private int DfsStack(Dictionary<string, DfsItem> graph, DfsItem currentDfsItem, Stack<string> order, int size, HashSet<string> visited)
+        {
+            Stack<DfsItem> stack = new Stack<DfsItem>();
+            string addToStack;
+            DfsItem dfsItem;
+
+            stack.Push(currentDfsItem);
+            while (stack.Count > 0)
+            {
+                addToStack = null;
+                dfsItem = null;
+
+                currentDfsItem = stack.Pop();
+                visited.Add(currentDfsItem.Key);
+                foreach (var edge in currentDfsItem.GetEdges())
+                {
+                    if(!visited.Contains(edge))
+                    {
+                        addToStack = edge;
+                        break;     
+                    }   
+                }
+                if (!string.IsNullOrEmpty(addToStack))
+                {
+                    stack.Push(currentDfsItem);
+                    if (graph.TryGetValue(addToStack, out dfsItem))
+                    {
+                        stack.Push(dfsItem);
+                    }
+                    else
+                    {
+                        visited.Add(addToStack);
+                        size++;
+                        order.Push(addToStack);
+                    }
+                }
+                else
+                {
+                    size++;
+                    order.Push(currentDfsItem.Key);
+                }
+            }
+            return size;
+        }
+
+        class DfsItem
+        {
+            public string Key { get; private set; }
+
+            HashSet<string> _edges;
+            
+            public DfsItem(string key, string edge)
+            {
+                this.Key = key;
+                _edges = new HashSet<string> { edge };
+            }
+
+            public void AddEdge(String node)
+            {
+                if(!_edges.Contains(node))
+                    _edges.Add(node);
+            }
+
+            public HashSet<string> GetEdges()
+            {
+                return _edges;
+            }
+        }
+    }
+}
---
+++ Week4/UnitTests/TestSccComputation.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SccComputation;
+
+namespace UnitTests
+{
+    
+    [TestClass]
+    public class TestSccComputation
+    {
+
+        [TestMethod]
+        public void TestComputeScc_test_file()
+        {
+            Main target = new Main();
+            var result = target.ComputeScc(@".\..\..\..\UnitTests\test.txt");
+
+            AssertArray(new int[] { 4, 3, 3, 1, 0 }, result);
+        }
+
+        [TestMethod]
+        public void TestComputeScc_test7_file()
+        {
+            Main target = new Main();
+            var result = target.ComputeScc(@".\..\..\..\UnitTests\test7.txt");
+
+            AssertArray(new int[] { 36, 7, 1, 1, 1 }, result);
+        }
+
+        [TestMethod]
+        public void TestComputeScc_test2_file()
+        {
+            Main target = new Main();
+            var result = target.ComputeScc(@".\..\..\..\UnitTests\test2.txt");
+
+            AssertArray(new int[] { 3, 3, 3, 0, 0 }, result);
+        }
+
+        [TestMethod]
+        public void TestComputeScc_test3_file()
+        {
+            Main target = new Main();
+            var result = target.ComputeScc(@".\..\..\..\UnitTests\test3.txt");
+
+            AssertArray(new int[] { 1, 1, 1, 0, 0 }, result);
+        }
+
+        [TestMethod]
+        public void TestComputeScc_real_file()
+        {
+            Main target = new Main();
+            var result = target.ComputeScc(@".\..\..\..\UnitTests\SCC.txt");
+
+            AssertArray(new int[] { 434821, 968, 459, 313, 211 }, result);
+        }
+
+        public void AssertArray(int[] expected, int[] actual)
+        {
+            Assert.AreEqual(expected.Count(), actual.Count());
+            for (int i = 0; i < expected.Count(); i++)
+            {
+                Assert.AreEqual(expected[i], actual[i]);
+            }
+        }
+    }
+}
